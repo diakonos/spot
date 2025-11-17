a115e6df632dc993fedf244cd1ef57a9145dde83
Justin Ling <jlinged@gmail.com>
2025-11-17 20:12:15 +0800

    fix workos webhooks

--- convex/http.ts
+++ convex/http.ts
@@ -1,5 +1,8 @@
 import { httpRouter } from "convex/server";
+import { registerWorkOSWebhooks } from "./workos/webhooks";
 
 const http = httpRouter();
+
+registerWorkOSWebhooks(http);
 
 export default http;
